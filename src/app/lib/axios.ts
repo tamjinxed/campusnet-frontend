8a6340b49ea55ba316ebf3d0f92040e47752fadc
Fahim <fahim.mun07@proton.me>
2025-06-21 01:15:45 +0600

    Changed the node env

--- src/app/lib/axios.ts
+++ src/app/lib/axios.ts
@@ -18,7 +18,10 @@ let failedRequestQueue: FailedRequestQueueItem[] = [];
 
 // Axios Instance
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL:
+        process.env.NODE_ENV === "production"
+            ? process.env.NEXT_PUBLIC_API_URL
+            : "http://localhost:4000/api/v1",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
